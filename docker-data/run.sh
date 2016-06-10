8d6ff749fc5f59d91db152f836d10236a970cd3f
aoki <aokinobu@gmail.com>
2016-06-10 13:10:41 +0900

    waiting for jar

--- docker-data/run.sh
+++ docker-data/run.sh
@@ -6,6 +6,8 @@ then
     echo waiting for web-${GTC_VERSION}.jar
     sleep 2
   done
+  echo waiting for jar compression
+  sleep 10
   rsync -avz /rdf.glytoucan/target/web-${GTC_VERSION}.jar /data/rdf.glytoucan/
   rsync -avz /glytoucan-stanza /data/
   rsync -avz /glytoucan-js-stanza /data/
@@ -14,12 +16,16 @@ then
     echo waiting for api-${GTC_VERSION}.jar
     sleep 2
   done
+  echo waiting for jar compression
+  sleep 10
   rsync -avz /api/target/api-${GTC_VERSION}.jar /data/api/
   while [ ! -f /soap.api/api.soap/target/soap-${GTC_VERSION}.jar ]
   do
     echo waiting for soap-${GTC_VERSION}.jar
     sleep 2
   done
+  echo waiting for jar compression
+  sleep 10
   rsync -avz /soap.api/api.soap/target/soap-${GTC_VERSION}.jar /data/soap.api/
   rsync -avz /pom-site /data/
 fi
